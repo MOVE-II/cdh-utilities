e202606c248f9bc8c8dd94f48e0df770b5d2d82f
karl.kraus <laqueray@googlemail.com>
2016-08-11 13:54:46 +0200

    added cmake toolchain files for bbb, atmel devboard (arm-blue)

---
+++ toolchain-bbb.cmake
@@ -0,0 +1,21 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# where is the target environment 
+SET(CMAKE_INSTALL_PREFIX ~/move-binaries/bbb CACHE PATH "installation path" FORCE)
+
+include_directories(~/move-binaries/bbb/include)
+link_directories(~/move-binaries/bbb/lib)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
