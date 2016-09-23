c614bc023befa351e25530ce958e3f0aac584843
Karl Kraus <karl.kraus@tum.de>
2016-09-23 18:35:59 +0200

    Fixed toolchain files, added toolchain file for generic linux builds

--- toolchain-bbb.cmake
+++ toolchain-bbb.cmake
@@ -4,18 +4,20 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # where is the target environment 
-SET(CMAKE_INSTALL_PREFIX ~/move-binaries/bbb CACHE PATH "installation path" FORCE)
 
-include_directories(~/move-binaries/bbb/include)
-link_directories(~/move-binaries/bbb/lib)
+SET(CMAKE_FIND_ROOT_PATH /opt/move-binaries/)
+SET(CMAKE_STAGING_PREFIX /opt/move-binaries/usr)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# prevent removing of RPATH variable from installed executables
+SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
---
+++ toolchain-linux.cmake
@@ -0,0 +1,3 @@
+
+# prevent removing of RPATH variable from installed executables
+SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
